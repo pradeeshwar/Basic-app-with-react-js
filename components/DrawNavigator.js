d8c1bd23d482ca0d1ad11773ad566854db2e3c17
pradeeshwar <68628319+pradeeshwar@users.noreply.github.com>
2020-09-22 08:34:59 +0530

    Update DrawNavigator.js

--- components/DrawNavigator.js
+++ components/DrawNavigator.js
@@ -100,7 +100,7 @@ const styles = StyleSheet.create(
           position: 'absolute',
          flexDirection: 'row',
           left: 0,
-         bottom: 10,
+         bottom: 0,
         top:  0 ,
           width: SlidingWidth,
       },
